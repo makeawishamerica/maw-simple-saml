samjaninf/simplesamlphp

Fix for Symfony4 & Catch environment from ENV variable

lib/SimpleSAML/Kernel.php
@@ -45,7 +45,9 @@ class Kernel extends BaseKernel
     {
         $this->module = $module;
 
-        parent::__construct('dev', false);
+        $env = getenv('SYMFONY_ENV') ?: (getenv('APP_ENV') ?: 'prod');
+
+        parent::__construct($env, false);
     }
 
 
@@ -153,6 +155,7 @@ class Kernel extends BaseKernel
         try {
             $definition = new Definition();
             $definition->setAutowired(true);
+            $definition->setPublic(true);
 
             $controllerDir = Module::getModuleDir($this->module) . '/lib/Controller';
 

makeawishamerica/maw-simple-saml

lib/SimpleSAML/Kernel.php
@@ -35,7 +35,9 @@ class Kernel extends BaseKernel
     {
         $this->module = $module;
 
-        parent::__construct('dev', false);
+        $env = getenv('APP_ENV') ?: (getenv('SYMFONY_ENV') ?: 'prod');
+
+        parent::__construct($env, false);
     }
 
 
@@ -143,6 +145,7 @@ class Kernel extends BaseKernel
         try {
             $definition = new Definition();
             $definition->setAutowired(true);
+            $definition->setPublic(true);
 
             $controllerDir = Module::getModuleDir($this->module) . '/lib/Controller';
 