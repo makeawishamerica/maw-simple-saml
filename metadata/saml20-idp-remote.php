3a63679c452d77d8ad19289c72ade9c4710faf96
Sharron Denice <sallen@wish.org>
2019-10-08 08:48:48 -0700

    Initial Commit
    - Wrapped two external apps, simple helloworld and certificate-admin-test.wish.org

---
+++ metadata/saml20-idp-remote.php
@@ -0,0 +1,53 @@
+<?php
+
+$metadata['https://sts.windows.net/c4a00911-19d1-4443-9717-cc9b02389a51/'] = array (
+  'entityid' => 'https://sts.windows.net/c4a00911-19d1-4443-9717-cc9b02389a51/',
+  'contacts' => 
+  array (
+  ),
+  'metadata-set' => 'saml20-idp-remote',
+  'SingleSignOnService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'https://login.microsoftonline.com/c4a00911-19d1-4443-9717-cc9b02389a51/saml2',
+    ),
+    1 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+      'Location' => 'https://login.microsoftonline.com/c4a00911-19d1-4443-9717-cc9b02389a51/saml2',
+    ),
+  ),
+  'SingleLogoutService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'https://login.microsoftonline.com/c4a00911-19d1-4443-9717-cc9b02389a51/saml2',
+    ),
+  ),
+  'ArtifactResolutionService' => 
+  array (
+  ),
+  'NameIDFormats' => 
+  array (
+  ),
+  'keys' => 
+  array (
+    0 => /* certificate-admin-test */
+    array (
+      'encryption' => false,
+      'signing' => true,
+      'type' => 'X509Certificate',
+      'X509Certificate' => 'MIIC8DCCAdigAwIBAgIQN33DV93L6aVB1tqClMkW5jANBgkqhkiG9w0BAQsFADA0MTIwMAYDVQQDEylNaWNyb3NvZnQgQXp1cmUgRmVkZXJhdGVkIFNTTyBDZXJ0aWZpY2F0ZTAeFw0xOTEwMDcxODI3NDFaFw0yMjEwMDcxODI3NDFaMDQxMjAwBgNVBAMTKU1pY3Jvc29mdCBBenVyZSBGZWRlcmF0ZWQgU1NPIENlcnRpZmljYXRlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwp9IB25xH1NuFClHQtUXr6TuZQy2fJcsdePgW3LJ3Pphh6gfnE/mNStJakZZXyjQGlnggxQKnchfzln+HyDnTUKOsQ64yuOKc14wSpsURmTX1a+SsZelDY9olJCt3Wtc+OAiONTcB5SqalpaZRVaWdBRxk8JYr0xZ/A7pkIbivGd/LuaKxUZNDfl2t6Ev+7ScvXRWpaYR2n1Jx6GXLjMFNx8TPhLcCJdxDNjssDObVuD2pq2Jtv8u6PbU0udlwK5TBhUO0HiDNrQslS0PaASJd55iFWhTADVO06IX2ML+P6vV+chOBtl+/JXZZCx37nf/E3F0eOXnqKduzI+09oU/wIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQCN3U1StPn+8l0e5854Ca6nbhewwWYBfvObYToua8BrWyHguiX7LbKXIL3fIr0X1RbvQYPSL5KDwZfO5r2xSQ1Ua3SrzYNKAhJVa/d8y+oXADvmSoFFCqS8d9xKJdqvNAXWUKUh8br0BTBXLZl5Lj82DoCV6+z3mn4/y60JQes992g0sKSFnD4Nwvw+AaTCulSjHDQFJwucPGkqdldXi97uzxnYG+t6xFF7bGrwOqzp27yUPBN5+Xtoz5znh3GPX9liQqc4bXc/ZJr+YlTdwRfbgr07jkCUUEDcl2ZaWPS7ofZ5DcY1oYg4gJAIwJAMFxu3GSy0s9xlWYFco6+v68Iz',
+    ),
+    1 => /* helloworld */
+    array (
+      'encryption' => false,
+      'signing' => true,
+      'type' => 'X509Certificate',
+      'X509Certificate' => 'MIIC8DCCAdigAwIBAgIQUbf7q7E7uodL+Kr/3HZK7TANBgkqhkiG9w0BAQsFADA0MTIwMAYDVQQDEylNaWNyb3NvZnQgQXp1cmUgRmVkZXJhdGVkIFNTTyBDZXJ0aWZpY2F0ZTAeFw0xOTEwMDQxOTIyMTBaFw0yMjEwMDQxOTIyMTBaMDQxMjAwBgNVBAMTKU1pY3Jvc29mdCBBenVyZSBGZWRlcmF0ZWQgU1NPIENlcnRpZmljYXRlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqQilupKLoj6cUXNBWnuc34aWVlT2nAdn4aZLw5lAerYFsvc9nJJ2k4xiTONXfA+myNfZQEQqh7UmQhN0bHJSbS+9B35A41qodtB2myX5zC2+X4HNVcgJcpH0X6C7DPlslQCZO0Ft8WmEoWKrOuQxVGg7cs41axZQJuSBAYImuik2d+IJjYBneQT+erT2dfuDAsb8ucRMTdZdK0iOh+/QeeFVWvB0ojQ/ZszYIB8l7oqXgDvbxkImXAqBU6pKrRaV0oqNWyU8S83MuYuadQy8ugV8qKZD5YtC76nx5glVWnMQF6SiMJR6jFiKfFG46ljMZMhk39U23OPDt+g4F0/ihQIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQAchNkkIo6oy7Q9Va0sGTszdbFe3eXLMVmnoNmQwqR6Xt1MS5+2h7j0bBEWd/mdLrnpUAtt7/Tsa2C1JqasEVqQw5l89yWYDO/LMUf9T0CVRe4gSfHbJhWdondOljxuc5ZEVnC/nWDlDzhdJnC2bqpQQNUenFmo3pLsqtIcSi+2m5IFcahCLUwAQlONBMZjO6+16zLaCDssgbfhhU4I3cUpJw3S4rjJcgM62obmcdCMSIZxIYc0MJysKY4TJJ0eV+8rj+H4Tp6D5mWZUytj+c7GU9+n2SPOfB31V2EE8uvJ+CpF2Q8t3Q/sqU0qSUx8DlPDdlT9jXeMs2e9zwpvgrbo',
+    ),
+  ),
+);
